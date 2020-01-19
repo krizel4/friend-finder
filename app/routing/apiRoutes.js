8872746e872a85bfb87fa4d08a00963f11b559a5
Nathan Minnema <Nathan.Minnema@fresnounified.org>
2020-01-18 23:40:28 -0800

    adding GET route for /api/friends

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+// HW instructions
+// * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+// * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+const friendsData = require("..app/data/friends");
+
+// Routes
+module.exports = function apiRoute (app) {
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(data)
+    });
+
+}
+
+
