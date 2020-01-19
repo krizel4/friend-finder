1ef418dd43ac665582fd4fb7977f22b69dcc0220
Krizel Minnema <krizel@Krizels-iMac.local>
2020-01-19 14:36:03 -0800

    fixing broken links with friends data and linked api routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
-let friends = [
+var friends = [
     {
-        "name":"Taylor Swift",
-        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/191125_Taylor_Swift_at_the_2019_American_Music_Awards.png/220px-191125_Taylor_Swift_at_the_2019_American_Music_Awards.png",
-        "scores":[
+        "name": "Taylor Swift",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/191125_Taylor_Swift_at_the_2019_American_Music_Awards.png/220px-191125_Taylor_Swift_at_the_2019_American_Music_Awards.png",
+        "scores": [
             5,
             1,
             4,
@@ -13,12 +13,12 @@ let friends = [
             5,
             4,
             1
-          ]
-      },
-      {
-        "name":"Selena Gomez",
-        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/191125_Selena_Gomez_at_the_2019_American_Music_Awards.png/220px-191125_Selena_Gomez_at_the_2019_American_Music_Awards.png",
-        "scores":[
+        ]
+    },
+    {
+        "name": "Selena Gomez",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/191125_Selena_Gomez_at_the_2019_American_Music_Awards.png/220px-191125_Selena_Gomez_at_the_2019_American_Music_Awards.png",
+        "scores": [
             2,
             1,
             4,
@@ -29,12 +29,12 @@ let friends = [
             4,
             3,
             1
-          ]
-      },
-      {
-        "name":"Ella Marija Lani Yelich-O'Connor ",
-        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg/220px-RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg",
-        "scores":[
+        ]
+    },
+    {
+        "name": "Ella Marija Lani Yelich-O'Connor ",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg/220px-RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg",
+        "scores": [
             1,
             2,
             3,
@@ -45,6 +45,8 @@ let friends = [
             3,
             4,
             5
-          ]
-      }
-]
+        ]
+    }
+];
+
+module.exports = friends;
