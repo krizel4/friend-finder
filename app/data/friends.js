53d348359e0414e8370ff859ba1b172b3aea6ea4
Krizel Minnema <krizel@Krizels-iMac.local>
2020-01-19 13:54:29 -0800

    adding friends to data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,50 @@
-// 1. Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
-
+let friends = [
+    {
+        "name":"Taylor Swift",
+        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/191125_Taylor_Swift_at_the_2019_American_Music_Awards.png/220px-191125_Taylor_Swift_at_the_2019_American_Music_Awards.png",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Selena Gomez",
+        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/191125_Selena_Gomez_at_the_2019_American_Music_Awards.png/220px-191125_Selena_Gomez_at_the_2019_American_Music_Awards.png",
+        "scores":[
+            2,
+            1,
+            4,
+            2,
+            2,
+            3,
+            1,
+            4,
+            3,
+            1
+          ]
+      },
+      {
+        "name":"Ella Marija Lani Yelich-O'Connor ",
+        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg/220px-RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5
+          ]
+      }
+]
