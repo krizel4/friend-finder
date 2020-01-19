d36395975f83711978a2aa549ee20359410e135c
Nathan Minnema <Nathan.Minnema@fresnounified.org>
2020-01-19 00:29:39 -0800

    adding compatibility logic and fixing syntax errors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,10 @@
 // * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 // * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-const friendsData = require("..app/data/friends");
+const friendsData = require(".app/data/friends.js");
 
 // Routes
-module.exports = function apiRoute (app) {
+module.exports = function apiRoute(app) {
 
     // Display a JSON of all possible friends.
     app.get("/api/friends", function (req, res) {
@@ -14,29 +14,38 @@ module.exports = function apiRoute (app) {
 
     // Compatibility logic.
     app.post("/api/friends", function (req, res) {
-        return res.json(friendsData);
+        // return res.json(friendsData);
 
         let newFriend = req.body.scores;
         let scoreArray = [];
         let friendMatch;
 
-            // Loop through buddies.
-            for(let i = 0; i < friendsData.length; i++){
+        // Loop through buddies.
+        for (let i = 0; i < friendsData.length; i++) {
 
-                // Compare buddy scores
-
-                // Push scores to array
-
-                // Find best match
-
-                // Show match
-
-                // Push submission to array
-
+            // Compare buddy scores
+            for (let j = i; j < newFriend.length; j++) {
+                compatibility += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriend[j])));
             }
 
-    })
+            // Push scores to array
+            scoreArray.push(compatibility);
+        }
 
+        // Find best match
+        for (let i = 0; i < scoreArray.length; i++) {
+            if (scoreArray[i] <= scoreArray[friendMatch]) {
+                friendMatch = i;
+            }
+        }
+
+        // Show match
+        let compatibilityFound = friendsData[friendMatch];
+        res.json(compatibilityFound);
+
+        // Push submission to array
+        friendsData.push(req.body);
+
+    });
 }
-
 
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-reformatrequire("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
