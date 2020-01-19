20b6730a77dce52a7f40dcd2d1a9fa88f4107acf
Nathan Minnema <Nathan.Minnema@fresnounified.org>
2020-01-18 23:54:06 -0800

    starting base for compatibility logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,20 @@ const friendsData = require("..app/data/friends");
 // Routes
 module.exports = function apiRoute (app) {
 
+    // Display a JSON of all possible friends.
     app.get("/api/friends", function (req, res) {
-        return res.json(data)
+        return res.json(data);
     });
+
+    // Compatibility logic.
+    app.post("/api/friends", function (req, res) {
+        return res.json(data);
+
+        let newFriend = req.body.scores;
+        let scoreArray = [];
+        let friendMatch;
+
+    })
 
 }
 
