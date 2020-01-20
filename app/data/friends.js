508fd5a58dcdda7c2ef94ad760a01f18b8113512
Krizel Minnema <krizel@Krizels-iMac.local>
2020-01-19 16:56:35 -0800

    turning scores into strings

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,48 +3,48 @@ var friends = [
         "name": "Taylor Swift",
         "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/191125_Taylor_Swift_at_the_2019_American_Music_Awards.png/220px-191125_Taylor_Swift_at_the_2019_American_Music_Awards.png",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
     },
     {
         "name": "Selena Gomez",
         "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/191125_Selena_Gomez_at_the_2019_American_Music_Awards.png/220px-191125_Selena_Gomez_at_the_2019_American_Music_Awards.png",
         "scores": [
-            2,
-            1,
-            4,
-            2,
-            2,
-            3,
-            1,
-            4,
-            3,
-            1
+            "2",
+            "1",
+            "4",
+            "2",
+            "2",
+            "3",
+            "1",
+            "4",
+            "3",
+            "1"
         ]
     },
     {
         "name": "Ella Marija Lani Yelich-O'Connor ",
         "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg/220px-RF_3006_Lorde%40Arena_Krists_Luhaers-5_%2835769377251%29_%28cropped%29.jpg",
         "scores": [
-            1,
-            2,
-            3,
-            4,
-            5,
-            1,
-            2,
-            3,
-            4,
-            5
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
         ]
     }
 ];
